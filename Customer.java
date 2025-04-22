3bbeede07bf1bf6f8a8f0a0ef2768e45eeab296f
AbdulrahmanSlaghour <abduslaghour@gmail.com>
2025-04-22 23:23:09 +0300

    Delete Customer.java

--- Customer.java
+++
@@ -1,9 +0,0 @@
-public abstract class Customer extends Person {
-    //***PROPERTIES***
-    //all properties inherited from Person
-    
-    //***BEHAVIOURS***
-    public Customer(int id, String name, String phoneNumber) {
-        super(id, name, phoneNumber);
-    }
-}
